e0058330c58333bb61f0101b245441e7a4d304c1
Shane O'Connor <spiderbaby@gmail.com>
2016-08-19 18:53:43 -0700

    Updating the import script to use SQLAlchemy classes.

--- Klab_LocalWebsite/Primer_upload_form.py
+++ Klab_LocalWebsite/Primer_upload_form.py
@@ -3,13 +3,18 @@ import sys
 import pprint
 import pandas as pd
 
+import sys
+sys.path.insert(0, '..')
+
+from model import Primers
+
 test_csv_path = 'biosensor_primers_v2.csv'
-delimiter = ','
+delimiter = '\t'
 
 print os.getcwd()
 
 # Import data
-user_input = pd.read_csv(test_csv_path, delimiter=delimiter)
+user_input = pd.read_csv(test_csv_path, delimiter='\t')
 
 # Model
 # class Primers(DeclarativeBasePlasmid):
@@ -27,41 +32,19 @@ user_input = pd.read_csv(test_csv_path, delimiter=delimiter)
 #     template_description = Column(Unicode(200), nullable = True)
 
 # Pseudo-model
-class Primers():
-    def __init__(self):
-        self.name = None
-        self.creator = None
-        self.sequence = None
-        self.direction = None
-        self.description = None
-        self.TM = None
-        self.date = None
-        self.GC = None
-        self.length = None
-        self.template_id = None
-        self.template_description = None
+
 
 # Parse and add to model
+print(user_input)
 for index, row in user_input.iterrows():
-    Primers.name = row[0]
-    Primers.creator = row[1]
-    Primers.sequence = row[2]
-    Primers.direction = row[3]
-    Primers.description = row[4]
-    Primers.TM = row[5]
-    Primers.date = row[6]
-    Primers.GC = row[7]
-    Primers.length = row[8]
-    Primers.template_id = row[9]
-    Primers.template_description = row[10]
+
+    d = {}
+    for header in ['creator', 'secondary_id', 'sequence', 'direction', 'description', 'TM', 'GC', 'length', 'template_id', 'template_description']:
+        d[header] = row[header]
+    db_record_object = Primers(**d)
+
     pprint.pprint(vars(Primers))
 
-# from sqlalchemy.orm import sessionmaker
-# Session = sessionmaker(bind=engine)
-# session = Session()
-#
-# plasmid = Plasmids(name='pJL001', user='James', sequence='ATCGATCGATCGATCGATCG')
-#
-# session.add(plasmid)
+
 
 
