957d6f4866fe137cdcc3352b3edb25c192c0cb88
Shane O'Connor <spiderbaby@gmail.com>
2016-08-19 18:52:49 -0700

    Updating schema (small change).

--- model.py
+++ model.py
@@ -23,6 +23,23 @@ class Primers(DeclarativeBasePlasmid):
     template_id = Column(Unicode(200, collation="utf8_bin"), nullable = True)
     template_description = Column(Unicode(200, collation="utf8_bin"), nullable = True)
 
+    def __repr__(self):
+        description = self.description or ''
+        if description:
+            description = '\n  Description: {0}'.format(description)
+        GC = self.GC or ''
+        if GC:
+            GC = ', {0}'.format(GC)
+        plength = self.length or ''
+        if plength:
+            plength = ', {0}'.format(plength)
+        s = 'Primer o{0}{1:04d} ({2}): {3}, {6}C, {7}{8} \n  Sequence: {4}{5}'.format(self.creator, self.creator_entry_number, self.secondary_id,
+                                                                    self.direction, self.sequence, description, self.TM, self.date, GC, plength)
+        if self.template_id:
+            s += '\n  Template #: {0}'.format(self.template_id)
+        if self.template_description:
+            s += '\n  Template: {0}'.format(self.template_description)
+        return s
 
 
 class Resistance(DeclarativeBasePlasmid):
@@ -37,3 +54,4 @@ class Users(DeclarativeBasePlasmid):
     ID = Column(Unicode(5, collation="utf8_bin"), nullable=False, primary_key=True)
     github_username = Column(Unicode(26, collation="utf8_bin"), nullable=False)
     lab_username = Column(Unicode(50, collation="utf8_bin"), nullable = False)
+    color = Column(Unicode(6, collation="utf8_bin"), nullable = False)
